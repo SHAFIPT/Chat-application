8abbebe5280c613aca0d16052a9e89e27c683bf2
SHAFIPT <shafipt077@gamil.com>
2024-11-19 21:53:14 +0530

    first-commit

---
+++ Client/src/App.tsx
@@ -0,0 +1,15 @@
+// import ChatPage from "./Pages/ChatPage"
+
+import LoginPage from "./Pages/LoginPage"
+
+
+const App = () => {
+  return (
+    <div>
+      {/* <ChatPage/> */}
+      <LoginPage/>
+    </div>
+  )
+}
+
+export default App
---
+++ Client/src/Pages/ChatPage.tsx
@@ -0,0 +1,10 @@
+
+const ChatPage = () => {
+  return (
+    <div>
+      <h1>hllow</h1>
+    </div>
+  )
+}
+
+export default ChatPage
